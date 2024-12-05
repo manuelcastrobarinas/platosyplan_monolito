1f4832e3bf3063c26811a09c49329af056954e5f
manuel castro barinas <manuel.castrobarinas@gmail.com>
2024-12-05 12:58:17 -0500

    update validate jwts

--- src/app/middleware/validate-jwt.ts
+++ src/app/middleware/validate-jwt.ts
@@ -3,21 +3,23 @@ import { Request, Response, NextFunction } from 'express';
 import { CONFIG } from '../../config';
 
 export function validateJwt(req: Request, res: Response, next: NextFunction) {
-  const token = req.header('x-token');
   try {
-    if (!token) return res.status(401).json({ ok: false, error_message: 'No hay un token en la petición' });
+    const authHeader = req.header('Authorization');
+    if (!authHeader) return res.status(401).json({ ok: false, error_message: 'No hay un token en la petición'});
+    if (!authHeader.startsWith('Bearer ')) return res.status(400).json({ok: false, error_message: 'Token con formato incorrecto'})
+  
+    const token = authHeader.split(' ')[1];
     const decoded = jwt.verify(token, CONFIG.jwt_key);
-
-    // Verifica si decoded es un objeto y contiene 'uid'
-    if (typeof decoded === 'object' && 'uid' in decoded) {
-      req.headers.uid = (decoded as JwtPayload).uid as string;
+    if (typeof decoded === 'object' && 'id' in decoded) {
+      req.headers.id = (decoded as JwtPayload).id as string;
       next();
-      return; // Detenemos la ejecución para evitar enviar dos respuestas
+      return;
     }
 
-    return res.status(401).json({ ok: false, error_message: 'Token no válido o no contiene uid' });
+    return res.status(401).json({ ok: false, error_message: 'Token no válido o no contiene id' });
   } catch (error) {
-    console.error('Token no válido:', error);
-    return res.status(401).json({ ok: false, error_message: 'Token no válido' });
+    console.error('Error en la validación del token:', error);
+    if (error instanceof jwt.TokenExpiredError) return res.status(401).json({ ok: false, error_message: 'El token ha expirado' });
+    return res.status(401).json({ ok: false, error_message: `Token no válido: ${error}` });
   }
 }
